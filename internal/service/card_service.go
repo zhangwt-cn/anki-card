e043636a89331561d0000d6912ea25c5d193e01e
zhangwt <im.zhangwt@gmail.com>
2024-08-14 16:20:20 +0800

    fix: Fix the material math formula that does not display

--- internal/service/card_service.go
+++ internal/service/card_service.go
@@ -45,7 +45,7 @@ func makeOpenAIRequest(c request.CreateCard) (*request.ChatCompletionResponse, e
 		Messages: []request.Message{
 			{
 				Role:    "system",
-				Content: "你是一个学者,你需要在以下材料中提取出所有的知识点转换为Anki卡片,不需要使用markdown语法,只返回生成后的json,json格式是[{\"question\":\"\",\"answer\":\"\"}]",
+				Content: "你是一个学者,你需要在以下材料中提取出所有的知识点转换为Anki卡片,如果材料中包含数学公式，也需要提取出来,只返回生成后的json,json格式是[{\"question\":\"\",\"answer\":\"\"}]",
 			},
 			{
 				Role:    "user",
